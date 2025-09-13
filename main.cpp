48fb97120dcdc7a54511f276b76a867647eeff16
Michael Linman <mllinman@msn.com>
2025-09-13 07:31:30 -0400

    Update Main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 
 // ...existing code...
 int main(int argc, char *argv[]) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
+    QApplication::setApplicationName("Shuriken_lite");
+    QApplication::setOrganizationName("BulletDrop_Studio");
+    QApplication::setApplicationVersion("1.0.0");
     // Load dark theme
     QFile qss(":/resources/darktheme.qss");
     if (qss.open(QFile::ReadOnly)) {
@@ -18,3 +22,21 @@ int main(int argc, char *argv[]) {
 
     return app.exec();
 }
+
+// If you need the installer-related code, move it into a function like below:
+
+/*
+bool createInstaller(const QString& appName, const QString& destExe, const QString& installDir,
+                     const QString& uninstallerPath, const QString& publisher, const QString& version,
+                     const QString& description, QString& log) {
+    // ...installer code here...
+    return true;
+}
+*/
+
+#include <QDir>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+
